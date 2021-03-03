c6ad49528ed3e4b327dc5d809147a9d6937d308a
TuyenBoss <phamtuyen5199@gmail.com>
2021-03-03 09:15:46 +0700

    first commit

---
+++ resources/lang/vn/footer.php
@@ -0,0 +1,14 @@
+<?php
+
+  return [
+    'AboutUs'          =>      'Về Chúng Tôi',
+    'address'          =>      '1027/39, Lê Hồng Phong, Phú Thọ, Thủ Dầu Một, Bình Dương.',
+    'FollowUs'         =>      'Theo Dõi Chúng Tôi',
+    'visa'             =>      'Thanh toán bằng thẻ Visa.',
+    'mastercard'       =>      'Thanh toán bằng thẻ MasterCard.',
+    'jcb'              =>      'Thanh toán bằng thẻ JCB.',
+    'cod'              =>      'Thanh toán khi nhận hàng.',
+    'nganluong'        =>      'Giải pháp thanh toán qua Ngân Lượng.',
+    'payment'          =>      'Hỗ Trợ Thanh Toán',
+    'support'          =>      'Hỗ Trợ Khách Hàng',
+  ];
---
+++ resources/views/admin/layouts/footer.blade.php
@@ -0,0 +1,8 @@
+<footer class="main-footer">
+  <!-- To the right -->
+  <div class="pull-right hidden-xs">
+    <b>TuyenPham</b> 2.4.18
+  </div>
+  <!-- Default to the left -->
+  <strong>Copyright TuyenPham &copy; 2020 <a href="#">PhoneStore</a>.</strong> PhamTuyen reserved.
+</footer>
